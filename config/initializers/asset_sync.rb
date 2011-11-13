f2cffa10caeb60de77229fb528ff03d52df0738d
Albert Lobo <albertlobo1981@yahoo.es>
2011-11-13 21:36:45 +0100

    x

---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,7 @@
+AssetSync.configure do |config|
+  config.aws_access_key = 'AKIAIZEOTRJNBXMU4ONQ'
+  config.aws_access_secret = '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'
+  config.aws_bucket = 'moleculardensity-assets'
+  config.aws_region = 'eu-west-1'
+  config.existing_remote_files = 'keep'
+end
