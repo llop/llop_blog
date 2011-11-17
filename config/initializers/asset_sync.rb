fcf24bb072e7b4352cee4a69aa7bf2b82379e21e
Albert Lobo <albertlobo1981@yahoo.es>
2011-11-17 17:39:03 +0100

    ahaha

--- config/initializers/asset_sync.rb
+++
@@ -1,8 +0,0 @@
-AssetSync.configure do |config|
-  config.aws_access_key_id = 'AKIAIZEOTRJNBXMU4ONQ'
-  config.aws_secret_access_key = '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'
-  config.fog_provider = 'AWS'
-  config.fog_directory = 'moleculardensity-assets'
-  config.fog_region = 'eu-west-1'
-  config.existing_remote_files = 'keep'
-end
