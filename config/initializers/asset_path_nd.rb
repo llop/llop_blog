a5a7b47ec914f3b24da96654d232d5b115df8a78
Albert Lobo <albertlobo1981@yahoo.es>
2011-11-17 17:40:47 +0100

    hehehe

---
+++ config/initializers/asset_path_nd.rb
@@ -0,0 +1,11 @@
+Sprockets::Helpers::RailsHelper.module_eval do
+  def digest_for(logical_path)
+      if digest_assets && asset_digests && (digest = asset_digests[logical_path])
+        return digest
+      end
+      if compile_assets && digest_assets && asset = asset_environment[logical_path]
+        return asset.digest_path
+      end
+      return logical_path  # Just return the freakin path!
+    end
+end
