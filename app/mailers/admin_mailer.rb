2a5965a70e5eb1adf2ab8b8a120653405bb8f8b5
Albert Lobo <albertlobo1981@yahoo.es>
2011-11-18 17:42:01 +0100

    asd

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,6 @@
 class AdminMailer < ActionMailer::Base
   default from: "Llop <albertlobo1981@gmail.com>"
-  default to: "albertlobo1981@gmail.com"
+  default to: "albertlobo1981@yahoo.es"
   
   def comment_created(comment)
     @comment = comment
