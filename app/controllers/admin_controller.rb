a6f013fdc8aaf3f4ba25d30003a96b0a95894775
Albert Lobo <albertlobo1981@yahoo.es>
2011-11-19 23:25:34 +0100

    http digest

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,14 @@ class AdminController < ApplicationController
   
   # Uh-uh heroku says PAY!
   # force_ssl
-  # Http basic authentication
-  http_basic_authenticate_with :name => ENV['MD_USER'], :password => ENV['MD_PASS'], :realm => 'moleculardensity'
+  # Http digest authentication
+  REALM = 'moleculardensity'
+  before_filter do |controller|
+    unless authenticate_with_http_digest(REALM) { |username| username == ENV['MD_USER'] ? ENV['MD_PASS'] : nil }
+      ActionController::HttpAuthentication::Digest.authentication_header(controller, REALM)
+      message = '<img src="<%= asset_path "401.jpg" %>" alt="" width="400" height="400" />'
+      controller.render :inline => message, :status => 401, :layout => "blog_error"
+    end
+  end
   
 end
--- config/initializers/http_auth_error_handler.rb
+++
@@ -1,7 +0,0 @@
-ActionController::HttpAuthentication::Basic.module_eval do
-  def authentication_request(controller, realm)
-    controller.headers["WWW-Authenticate"] = %(Basic realm="#{realm.gsub(/"/, "")}")
-    message = '<img src="<%= asset_path "401.jpg" %>" alt="" width="400" height="400" />'
-    controller.render :inline => message, :status => 401, :layout => "blog_error"
-  end
-end
