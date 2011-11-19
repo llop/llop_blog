95ca908bd3505ea1b74eae7a8efd7cfc296adb0b
Albert Lobo <albertlobo1981@yahoo.es>
2011-11-19 13:32:28 +0100

    z

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,8 +3,6 @@ class AdminController < ApplicationController
   # Uh-uh heroku says PAY!
   # force_ssl
   # Http basic authentication
-  ENV['MD_USER'] ||= 'llopi'
-  ENV['MD_PASS'] ||= 'sepense'
   http_basic_authenticate_with :name => ENV['MD_USER'], :password => ENV['MD_PASS'], :realm => 'moleculardensity'
   
 end
