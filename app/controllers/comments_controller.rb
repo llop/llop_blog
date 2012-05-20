b16e867e0afc7850d42f80e4d9a7ade4fba4884c
Albert Lobo <albertlobo1981@yahoo.es>
2012-05-20 11:44:29 +0200

    Shit ip

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,13 @@ class CommentsController < BlogController
   
   # POST /comments
   def create
+    # Shit ips list
+    shit_ips = [ "188.138.84.93", "62.75.181.210" ]
+    
     @post = Post.find params[:post_id]
     @comment = Comment.new params[:comment]
     @comment.post = @post
-    if @comment.save
+    if (shit_ips.include?(request.remote_ip) || @comment.save)
       redirect_to post_path(@post)
     else
       render "posts/show"
