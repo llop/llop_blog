20878ee52ca99eeafbb0383d5d313d873dc7d16f
Albert Lobo <albertlobo1981@yahoo.es>
2012-05-25 00:53:38 +0200

    zxczc

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,12 +1,21 @@
 class BlogController < ApplicationController
   
   # Filters
+  before_filter :filter_shit_ips
   before_filter :load_categories
   before_filter :load_tag_cloud
   before_filter :load_archives
   
   # Protected methods
 protected
+  
+  def filter_shit_ips
+    # Quick deal with shit ips
+    shit_ips = [ "217.172.180.18", "62.75.181.210" ]
+    if shit_ips.include?(request.remote_ip)
+      render :inline => "<html><head><meta http-equiv=\"Refresh\" content=\"0; url=http://www.sandnes-sykleklubb.no\" /></head><body></body></html>", :status => 200
+    end
+  end
   
   def load_categories
     @categories = Category.order_by_name_asc_cached
